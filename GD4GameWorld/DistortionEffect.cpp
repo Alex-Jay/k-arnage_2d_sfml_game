e03b214d09975639075a02db3ddef16ff974d72c
Michael <mgrin10@student.dkit.ie>
2019-03-16 00:56:07 +0000

    Code Integration Add Distortion Effect

---
+++ GD4GameWorld/DistortionEffect.cpp
@@ -0,0 +1,115 @@
+#include "DistortionEffect.hpp"
+#include <iostream>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Err.hpp>
+#include <SFML/System/Clock.hpp>
+#include <Book/Utility.hpp>
+
+
+// Frag File and tutorial from Here https://www.lucidarme.me/sfml-distorsion-shader/
+
+DistortionEffect::DistortionEffect()
+	: mDistortionFactor(0.05f)
+	, mDistortionSpeed(0.1f)
+{
+	//Had trouble loading shader from mShaders, due to second parameter of (shaderType) required
+	//mShaders.load(Shaders::Distortion, "Media/Shaders/Distortion.frag", sf::Shader::Fragment);
+	if (!mShader.loadFromFile("Media/Shaders/Distortion.frag", sf::Shader::Fragment))
+	{
+		throw std::runtime_error("Failed to load Media/Shaders/Distortion.frag");
+	}
+
+	mShaders.load(Shaders::AddPass, "Media/Shaders/Fullpass.vert", "Media/Shaders/Add.frag");
+}
+
+void DistortionEffect::setTextureMap(const TextureHolder & textures)
+{
+	mDistortionMap = textures.get(Textures::ID::DistortionMap);
+	mDistortionMap.setRepeated(true);
+	mDistortionMap.setSmooth(true);
+	mShader.setUniform("currentTexture", sf::Shader::CurrentTexture);
+	mShader.setUniform("distortionMapTexture", mDistortionMap);
+}
+
+void DistortionEffect::apply(const sf::RenderTexture& input, sf::RenderTarget& output)
+{
+
+	//Set Sprites texture to the input, this will be the water background
+	mSprite.setTexture(input.getTexture());
+	//centreOrigin(mSprite);
+
+	//Set Properties of the Distortion shader
+	//Time controls the movement of the distortion effect
+	mShader.setUniform("time", mTimer.getElapsedTime().asSeconds());
+	//DistortionFactor is the amount of distortion applied
+	mShader.setUniform("distortionFactor", mDistortionFactor);
+	//DistortionSpeed is the speed of the distortion
+	mShader.setUniform("distortionSpeed", mDistortionSpeed);
+
+	output.clear();
+	//Center the Sprite Position
+	mSprite.setPosition(output.getSize().x / 2., output.getSize().y / 2.);
+	output.draw(mSprite, &mShader);
+
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++
+//Below is commented out version of the apply() method that I could not get to work as I struggled to understand the implimentation of the code.
+//This is where I was trying to call applyShader in PostEffect Using Vertex arrays.
+//if below code is uncommented and above 3 lines commented out, it draws the watertexture but with no movement
+
+//void DistortionEffect::apply(const sf::RenderTexture& input, sf::RenderTarget& output)
+//{
+//
+//	//Set Sprites texture to the input, this will be the water background
+//	mSprite.setTexture(input.getTexture());
+//	centreOrigin(mSprite);
+//
+//	prepareTextures(input.getSize());
+//
+//	distort(mFirstPassTextures[0]);
+//	distort(mSecondPassTextures[0]);
+//
+//	add(mFirstPassTextures[0], mSecondPassTextures[0], mFirstPassTextures[1]);
+//	mFirstPassTextures[1].display();
+//	add(input, mFirstPassTextures[1], output);
+//}
+
+void DistortionEffect::prepareTextures(sf::Vector2u size)
+{
+	if (mDistortionTexture.getSize() != size)
+	{
+		mDistortionTexture.create(size.x, size.y);
+		mDistortionTexture.setSmooth(true);
+
+		mFirstPassTextures[0].create(size.x / 2, size.y / 2);
+		mFirstPassTextures[0].setSmooth(true);
+		mFirstPassTextures[1].create(size.x / 2, size.y / 2);
+		mFirstPassTextures[1].setSmooth(true);
+
+		mSecondPassTextures[0].create(size.x / 4, size.y / 4);
+		mSecondPassTextures[0].setSmooth(true);
+		mSecondPassTextures[1].create(size.x / 4, size.y / 4);
+		mSecondPassTextures[1].setSmooth(true);
+	}
+}
+
+void DistortionEffect::distort(sf::RenderTexture& output)
+{
+
+	mShader.setUniform("time", mTimer.getElapsedTime().asSeconds());
+	mShader.setUniform("distortionFactor", mDistortionFactor);
+	mShader.setUniform("distortionSpeed", mDistortionSpeed);
+	applyShader(mShader, output);
+	output.display();
+}
+
+
+void DistortionEffect::add(const sf::RenderTexture& source, const sf::RenderTexture& distort, sf::RenderTarget& output)
+{
+	sf::Shader& adder = mShaders.get(Shaders::AddPass);
+
+	adder.setUniform("source", source.getTexture());
+	adder.setUniform("distort", distort.getTexture());
+	applyShader(adder, output);
+}
---
+++ GD4GameWorld/DistortionEffect.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <Book/PostEffect.hpp>
+#include <Book/ResourceIdentifiers.hpp>
+#include <Book/ResourceHolder.hpp>
+
+#include <SFML/Graphics/RenderTexture.hpp>
+#include <SFML/Graphics/Shader.hpp>
+
+#include <array>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Clock.hpp>
+
+
+class DistortionEffect : public PostEffect
+{
+public:
+	DistortionEffect();
+
+	virtual void apply(const sf::RenderTexture& input, sf::RenderTarget& output);
+
+	void setTextureMap(const TextureHolder & textures);
+
+private:
+	typedef std::array<sf::RenderTexture, 2> RenderTextureArray;
+
+	void prepareTextures(sf::Vector2u size);
+	void distort(sf::RenderTexture& output);
+	void add(const sf::RenderTexture& source, const sf::RenderTexture& distort, sf::RenderTarget& output);
+private:
+
+	sf::Shader mShader;
+	sf::Sprite mSprite;
+	sf::Texture mDistortionMap;
+	sf::Clock mTimer;
+
+	ShaderHolder mShaders;
+
+	sf::RenderTexture mDistortionTexture;
+	RenderTextureArray mFirstPassTextures;
+	RenderTextureArray mSecondPassTextures;;
+
+
+	float mDistortionFactor;
+	float mDistortionSpeed;
+};
